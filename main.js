26a5d640574189f3e5fa065e7f0dc3242180a90f
titi <tata@gmail.com>
2024-11-22 08:15:53 +0100

    EXO7

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
+function addition(param1,param2){
+    return param1 + param2;
+}
+
+
 var maVarible = parseInt(prompt("Donner un nombre"));
 var resultat = 0;
 resultat+=maVarible;
 
 for(var i=1;i<maVarible;i++)
 {
-    resultat+=i;
+    resultat=addition(resultat,i);
 }
 
 if(resultat <= 0){
